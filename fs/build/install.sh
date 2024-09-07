58a1b227938e685d30a5802fc52316e7c6760de8
Doug Elkin <doug@dougelkin.com>
2024-09-07 02:06:57 -0400

    import dogsay from host

--- fs/build/install.sh
+++ fs/build/install.sh
@@ -38,6 +38,7 @@ function _fix_permissions {
 function _import_host_commands {
   ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/borg
   ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/docker
+  ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/dogsay
   ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/flatpak
   ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/podman
   ln -fs /usr/bin/distrobox-host-exec /usr/local/bin/rpm-ostree
